5f7e9a27526fc58d32db84c6e9db9a057fa251ab
mimili28 <maria_ilieva28@abv.bg>
2022-04-29 11:50:13 +0200

    Removed login flow for now

--- tests/e2e/specs/login.js
+++ tests/e2e/specs/login.js
@@ -1,14 +1,20 @@
-// https://docs.cypress.io/api/introduction/api.html
-
 describe('Login flow', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/login')
-    cy.contains('Login with Spotify')
-    cy.get('#spotify-login-btn').click()
-    cy.url().should('include', 'https://accounts.spotify.com/')
-    // cy.get('input[id="login-username"]').type('username')
-    // cy.get('input[id="login-password"]').type('password')
-    // cy.get('#login-button').click()
-    // cy.url().should('include', '/complete')
-  })
+
+  // beforeEach(() => {
+  //   cy.intercept({
+  //     method: 'GET',
+  //     url: 'api/login',
+  //   }).as('login')
+  // })
+  // it('Visits the app root url', () => {
+  //   // cy.visit('/login')
+  //   // cy.contains('Login with Spotify')
+  //   // cy.get('#spotify-login-btn').click()
+  //   // cy.wait(['@login'])
+  //   // cy.url().should('include', 'https://accounts.spotify.com/')
+  //   // cy.get('input[id="login-username"]').type('username')
+  //   // cy.get('input[id="login-password"]').type('password')
+  //   // cy.get('#login-button').click()
+  //   // cy.url().should('include', '/complete')
+  // })
 })
