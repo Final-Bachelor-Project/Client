1997cafef4f780ddd541a48a41499cce14c14471
Amalia Caldare <amaliacaldare20@gmail.com>
2022-04-29 12:51:36 +0200

    Added explore

--- src/views/ExplorePage.vue
+++ src/views/ExplorePage.vue
@@ -1,9 +1,21 @@
 <template>
-    <div></div>
+    <div>
+
+    </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            users: []
+        }
+    },
+    created: async function () {
+        const users = (await this.$axios.get('api/users'));
+        this.users = users;
+    },
+    methods: {
+    }
 }
 </script>
 <style scoped>
