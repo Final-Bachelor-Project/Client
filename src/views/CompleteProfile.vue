ef2f1b95800d1734adc69ffec99d31ac11c11a74
mimili28 <maria_ilieva28@abv.bg>
2022-05-13 14:52:33 +0200

    Removed test

--- src/views/CompleteProfile.vue
+++ src/views/CompleteProfile.vue
@@ -94,9 +94,8 @@ export default {
         }
     },
     created: async function () {
-        const test = (await this.$axios.get('/api/login/test')).data
-        console.log(test);
         const currentUser = (await this.$axios.get('/api/users/current', {withCredentials: true})).data.user
+        console.log(currentUser);
         this.user = {
             spotifyUserId: currentUser.id,
             username: currentUser.display_name,
