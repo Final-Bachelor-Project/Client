31df564a32fb5b3dfecb54cfc7a5700e44401f13
mimili28 <maria_ilieva28@abv.bg>
2022-05-13 12:21:16 +0200

    Fix to url

--- src/views/CompleteProfile.vue
+++ src/views/CompleteProfile.vue
@@ -94,7 +94,7 @@ export default {
         }
     },
     created: async function () {
-      console.log(await this.$axios.get('http://167.172.189.64/api/users/current'));
+      console.log(await this.$axios.get('http://207.154.223.42/api/users/current'));
         const currentUser = (await this.$axios.get('http://167.172.189.64/api/users/current')).data.user
         this.user = {
             spotifyUserId: currentUser.id,
